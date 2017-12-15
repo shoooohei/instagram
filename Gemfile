883a21d711cc6a67a0e6299ec081df8f5bcbfe13
shohei <shoheimoment@gmail.com>
2017-12-15 09:26:44 +0900

    test

--- Gemfile
+++ Gemfile
@@ -37,7 +37,8 @@ gem 'bcrypt', '3.1.11'
 
 gem 'carrierwave'
 gem 'mini_magick'
-gem 'aws-sdk', '~> 2'
+gem 'fog-aws'
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -6,12 +6,15 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   # storage :fog
-  storage :file
 
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  if Rails.env.development?
+    storage :file
+
+    # Override the directory where uploaded files will be stored.
+    # This is a sensible default for uploaders that are meant to be mounted:
+    def store_dir
+      "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    end
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
