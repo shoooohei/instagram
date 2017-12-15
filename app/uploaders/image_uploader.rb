51bf345a17ecaa86e6f7e35b5ae1fc4e1e9465f9
shohei <shoheimoment@gmail.com>
2017-12-15 09:42:20 +0900

    hoget

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,15 +7,19 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   # storage :fog
 
-  if Rails.env.development?
+
+  if Rails.env.production?
+    storage :fog
+  else
     storage :file
+  end
 
     # Override the directory where uploaded files will be stored.
     # This is a sensible default for uploaders that are meant to be mounted:
     def store_dir
       "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
     end
-  end
+
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
